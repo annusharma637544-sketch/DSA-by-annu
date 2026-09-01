7590aef56be6f4383602e6a054353140ef4309ce
annusharma637544-sketch <annusharma637544@gmail.com>
2026-09-01 21:20:25 +0530

    calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"enter value of a"<<endl;
+    cin>>a;
+    cout<<"enter value of b"<<endl;
+    cin>>b;
+    char op;
+    cout<<"Enter operation="<<endl;
+    cin>>op;
+
+    switch(op){
+case '+': cout<<(a+b)<<endl;
+break;
+case '-': cout<<(a-b)<<endl;
+break;
+case '*': cout<<(a*b)<<endl;
+break;
+case '/': cout<<(a/b)<<endl;
+break;
+case '%': cout<<(a%b)<<endl;
+break;
+
+default: cout<<"please enter a valid operation"<<endl;
+    }
+        return 0;
+    
+}
