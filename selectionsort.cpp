4bfbe405261caf027bd6ec7ba911c6dd972d4910
annusharma637544-sketch <annusharma637544@gmail.com>
2026-09-12 17:43:24 +0530

    selectionsort.cpp

---
+++ selectionsort.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+void selectionsort(int arr[],int n){
+    for(int i=0; i<n-1; i++){
+        int smallestindex=i;
+        for(int j=i+1; j<n; j++){
+            if(arr[j]<arr[smallestindex]){
+                smallestindex=j;
+            }
+        }
+           swap(arr[i], arr[smallestindex]);
+    }
+}
+int main(){
+    int n;
+    cout<<"enter n=";
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+        selectionsort(arr,n);
+        
+        for(int i=0; i<n; i++){
+            cout<<arr[i]<<" ";
+        }
+           return 0;
+    }
+    
