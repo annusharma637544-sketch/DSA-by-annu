c8cb3e44318971c5c329400b93fdb7c9e45369e7
annusharma637544-sketch <annusharma637544@gmail.com>
2026-09-21 21:28:35 +0530

    maxconsecutiveones.cpp

---
+++ maxconsecutiveones.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int maxconsecutiveones(vector<int> nums) {
+
+    int maxi = 0;
+    int count = 0;
+
+    for (int i = 0; i < nums.size(); i++) {
+
+        if (nums[i] == 1) {
+            count++;
+            maxi = max(maxi, count);
+        }
+        else {
+            count = 0;
+        }
+    }
+
+    return maxi;
+}
+
+int main() {
+
+    int n;
+
+    cout << "Enter no. of elements in an array = ";
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int ans = maxconsecutiveones(arr);
+
+    cout << "Maximum consecutive 1s = " << ans;
+
+    return 0;
+}
